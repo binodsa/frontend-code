c8f79435d6b9f52d5019103d5598e0e96267d9e2
binodsa <159367694+binodsa@users.noreply.github.com>
2024-12-25 18:24:42 +0000

    initial commit

--- index.js
+++ index.js
@@ -3,15 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Hook_ControlledButtonState from './Counter';
 import EmojeeCounter from './EmojeeCounters';
-import DynamicEmoji from './DynamicEmoji';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Hook_ControlledButtonState />
-    <EmojeeCounter pic="Love" />
-    <EmojeeCounter pic="Sad" />
-    <EmojeeCounter pic="Like" />
-    <DynamicEmoji />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <Hook_ControlledButtonState />
+        <EmojeeCounter pic="Love" />
+        <EmojeeCounter pic="Sad" />
+        <EmojeeCounter pic="Like" />
+    </React.StrictMode>,
+    document.getElementById('root')
 );
